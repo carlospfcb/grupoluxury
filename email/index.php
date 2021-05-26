60573450dba7c1868ade3c5148378d1e1fe0268a
Eliezer Daniel Garcia <eliezerdanielgh1@gmail.com>
2021-05-25 20:44:25 -0400

    cambios

--- email/index.php
+++ email/index.php
@@ -1,6 +1,15 @@
 <?php
+
+
 require 'PHPMailerAutoload.php';
 date_default_timezone_set('Etc/UTC');
+
+$the_subject = $_POST['subject'];
+//$address_to = "eliezerdanielgh1@gmail.com";
+$address_to = $_POST['email'];
+$name =   $_POST['name'];
+$message = $_POST['message'];
+$from_name = "Conctato interesado";
 
 //Create a new PHPMailer instance
 $mail = new PHPMailer();
@@ -9,16 +18,16 @@ $mail->IsSMTP();
 //Configuracion servidor mail
 // $mail->From = "mail@gmail.com"; 
 $mail->SMTPAuth = true;
-$mail->SMTPSecure = 'tls'; //seguridad
-$mail->Host = "smtp.gmail.com"; // servidor smtp
-$mail->Port = 587; //puerto
+// $mail->SMTPSecure = 'tls'; //seguridad
+$mail->Host = 'smtp.gmail.com'; // servidor smtp
+$mail->Port = 465; //puerto
 $mail->Username ='sistemaflujodecaja@gmail.com'; //nombre usuario
 $mail->Password = 'sistema1234'; //contraseña
  
 //Agregar destinatario
-$mail->AddAddress('eliezerdanielgh1@gmail.com');
-$mail->Subject = 'hola';
-$mail->Body = 'mensaje';
+$mail->AddAddress($address_to);
+$mail->Subject = $the_subject;
+$mail->Body = $message;
  
 //Avisar si fue enviado o no y dirigir al index
 if ($mail->Send()) {
